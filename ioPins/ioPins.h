f9844fa16323483c41889f044bd700f5043ee9f6
Natani Simak <matej.simacek.2@gmail.com>
2025-03-11 17:28:34 +0100

    Pins.
    i/o pins for mk4 pcb.

---
+++ ioPins/ioPins.h
@@ -0,0 +1,42 @@
+/**
+ * GPIOs for the SUMEC MK 4.
+ */
+
+#define S_PIN_LED_1 21
+#define S_PIN_LED_2 14
+#define S_PIN_LED_3 13
+
+#define S_PIN_BUTTON_3 48
+#define S_PIN_BUTTON_4 47
+
+
+#define S_PIN_QRE_A 6
+#define S_PIN_QRE_B 7
+#define S_PIN_QRE_C 2
+
+#define S_PIN_SHARP_1 8
+#define S_PIN_SHARP_2 4
+
+#define S_PIN_POL_PWM_LEFT  16
+#define S_PIN_POL_PWM_RIGHT 5
+#define S_PIN_POL_PWM_MID   15
+
+
+#define S_PIN_SDA 17
+#define S_PIN_SCL 18
+
+
+#define S_PIN_IR_MODULE 35
+
+
+#define S_PIN_ENC_LEFT_A 9
+#define S_PIN_ENC_LEFT_B 10
+
+#define S_PIN_ENC_RIGHT_A 12
+#define S_PIN_ENC_RIGHT_B 11
+
+#define S_PIN_MOTOR_N_SLEEP  38
+#define S_PIN_MOTOR_LEFT_EN  42
+#define S_PIN_MOTOR_LEFT_PH  40
+#define S_PIN_MOTOR_RIGHT_EN 39
+#define S_PIN_MOTOR_RIGHT_PH 41
