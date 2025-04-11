720e516cd57eb11bec594ed070fbb1ef86da2b6c
Natani Simak <matej.simacek.2@gmail.com>
2025-04-11 12:27:50 +0200

    defined pins and the buffer

--- mainPrograms/startModule/src/main.cpp
+++ mainPrograms/startModule/src/main.cpp
@@ -11,26 +11,42 @@
 #include <Arduino.h>
 
 
+#define PIN_STATE_OUT DDB3
+#define PIN_IR_INPUT  DDB4
+#define PIN_LED1      DDB1
+#define PIN_LED2      DDB0
 
-struct ringBuffer {
-private:
-  uint16_t      buffer     = 0;
-  uint8_t       indexRead  = 0;
-  uint8_t       indexWrite = 0;
-  const uint8_t bufferSize = 14;
 
-public:
-};
+uint64_t             buffer                 = 0;
+static const uint8_t bufferSize             = sizeof(buffer) * 8;
+uint32_t             timeBuffer[bufferSize] = {0};
+uint32_t             timeSinceLastTrig          = 0;  // Millis.
+uint8_t              buffIndex                  = 0;  // Micros.
 
+
+void interrupt() {
+  if(buffIndex + 1 >= bufferSize) {
+    return;
+  }
+
+  timeSinceLastTrig = millis();
+  
+  buffIndex++;
+}
 
 
 void setup() {
-  DDRB |= 1 << DDB3;  // Set 3 as output.
-  DDRB &= 0 << DDB4;  // Set 4 as input.
-
-  PORTB |= 1 << DDB3;
+  cli();
+  DDRB |= 1 << PIN_STATE_OUT;  // Set 3 as output.
+  DDRB &= 0 << PIN_IR_INPUT;   // Set 4 as input.
+#if FINAL == 1
+  DDRB |= 1 << PIN_LED1;  // Set 1 as output.
+  DDRB |= 1 << PIN_LED2;  // Set 0 as output.
+#endif
+  attachInterrupt(PIN_IR_INPUT, interrupt, CHANGE);
+  /*PORTB |= 1 << DDB3;
   PORTB &= 0 << DDB3;
-  (PORTB >> DDB3) & 1;
+  (PORTB >> DDB3) & 1;*/
   sei();
 }
 
