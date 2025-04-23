9d0968a1ddd5f36565cca76a095972588651b2b7
Roselyn Natani Šimáčková <ms.natani.sim@gmail.com>
2025-04-23 19:10:35 +0200

    Added mk5, updated mk4

--- ioPins/ioPinsMk4.h
+++ ioPins/ioPinsMk4.h
@@ -5,21 +5,26 @@
 #define S_PIN_LED_1 21
 #define S_PIN_LED_2 14
 #define S_PIN_LED_3 13
+#define S_PIN_LED_4 0xff
 
-#define S_PIN_BUTTON_3 48
-#define S_PIN_BUTTON_4 47
+#define S_PIN_BUTTON_3 		48
+#define S_PIN_BUTTON_3_MODE INPUT
+#define S_PIN_BUTTON_4 		47
+#define S_PIN_BUTTON_4_MODE INPUT
 
 
-#define S_PIN_QRE_A 6
-#define S_PIN_QRE_B 7
-#define S_PIN_QRE_C 2
+// Order might be wrong.
+#define S_PIN_QRE_L 6
+#define S_PIN_QRE_R 7
+#define S_PIN_QRE_B 2
 
-#define S_PIN_SHARP_1 8
-#define S_PIN_SHARP_2 4
+// Order might be wrong.
+#define S_PIN_SHARP_L 8
+#define S_PIN_SHARP_R 4
 
-#define S_PIN_POL_PWM_LEFT  16
-#define S_PIN_POL_PWM_RIGHT 5
-#define S_PIN_POL_PWM_MID   15
+#define S_PIN_POL_PWM_L	16
+#define S_PIN_POL_PWM_R 5
+#define S_PIN_POL_PWM_M	15
 
 
 #define S_PIN_SDA 17
@@ -36,7 +41,11 @@
 #define S_PIN_ENC_RIGHT_B 11
 
 #define S_PIN_MOTOR_N_SLEEP  38
+// Pwm.
 #define S_PIN_MOTOR_LEFT_EN  42
+// In2.
 #define S_PIN_MOTOR_LEFT_PH  40
+// Pwm.
 #define S_PIN_MOTOR_RIGHT_EN 39
+// In2.
 #define S_PIN_MOTOR_RIGHT_PH 41
